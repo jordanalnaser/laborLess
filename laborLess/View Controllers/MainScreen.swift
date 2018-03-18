e6dcaa850fd743fc0f89dc783dce758ea2539746
Jordan Alnaser <jordanalnaser@Jordans-MacBook-Pro.local>
2018-03-18 13:19:55 -0500

    Creating Main Screen View Controller

---
+++ laborLess/View Controllers/MainScreen.swift
@@ -0,0 +1,22 @@
+//
+//  MainScreen.swift
+//  laborLess
+//
+//  Created by Jordan Alnaser on 3/18/18.
+//  Copyright © 2018 Jordan Alnaser. All rights reserved.
+//
+
+import UIKit
+
+
+class MainScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+    
+}
